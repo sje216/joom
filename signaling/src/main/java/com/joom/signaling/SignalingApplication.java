6f6ba32201676a28c0f4d547edee9384e594959f
Jieun Shin <jieunshin9633@gmail.com>
2026-02-23 16:48:51 +0900

    init spring boot project

---
+++ signaling/src/main/java/com/joom/signaling/SignalingApplication.java
@@ -0,0 +1,13 @@
+package com.joom.signaling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalingApplication.class, args);
+	}
+
+}
---
+++ signaling/src/test/java/com/joom/signaling/SignalingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joom.signaling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
